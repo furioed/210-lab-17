7e2c70f78d37fd82e924178a9772763302c644f0
Mamadou Sissoko <msissoko2317@gmail.com>
2025-10-02 17:45:05 -0700

    Lab 17 Update 3 and Final Submission

--- 210-lab-17.cpp
+++ 210-lab-17.cpp
@@ -13,61 +13,90 @@ struct Node {
 };
 
 // function prototypes
-void addToFront(Node*& head, int val);   // add new node at front
-void addToTail(Node*& head, int val);    // add new node at tail
-void output(Node* head);                 // display linked list
-void deleteNode(Node*& head, int pos);   // delete node at position
-void deleteList(Node*& head);            // delete entire linked list
+void addToFront(Node*& head, int val);          // add new node at front
+void addToTail(Node*& head, int val);           // add new node at tail
+void insertNode(Node*& head, int pos, int val); // insert node at position
+void deleteNode(Node*& head, int pos);          // delete node at position
+void output(Node* head);                         // display linked list
+void deleteList(Node*& head);                    // delete entire linked list
 
 int main() {
     srand(time(0));
-    Node* head = nullptr;  // pointer to first node
+    Node* head = nullptr; // pointer to first node
 
     // create a linked list of size 7 with random numbers 0-99
-    for (int i = 0; i < 4; i++) addToFront(head, rand() % 100); // first half front
-    for (int i = 0; i < 3; i++) addToTail(head, rand() % 100);  // second half tail
+    for (int i = 0; i < 3; i++) addToFront(head, rand() % 100); // first part front
+    for (int i = 0; i < 4; i++) addToTail(head, rand() % 100);  // second part tail
 
     cout << "Initial list" << endl;
     output(head); // display current list
 
-    // deleting a node
+    // insert a node
     int entry;
-    cout << "Which node to delete? " << endl; // prompt user
-    output(head); // show list for reference
+    cout << "After which node to insert 10000? " << endl; // prompt user
+    output(head); // show current list
     cout << "Choice --> ";
     cin >> entry;
-    deleteNode(head, entry); // remove selected node
+    insertNode(head, entry, 10000); // insert new node after chosen position
+
+    cout << "List after insertion" << endl;
+    output(head); // display updated list
+
+    // delete a node
+    cout << "Which node to delete? " << endl; // prompt user
+    output(head); // show current list
+    cout << "Choice --> ";
+    cin >> entry;
+    deleteNode(head, entry); // delete chosen node
 
     cout << "List after deletion" << endl;
-    output(head); // show updated list
+    output(head); // display final list
 
     // delete the entire linked list
-    deleteList(head); // free memory for all remaining nodes
+    deleteList(head); // free all nodes
 
-    cout << "Empty list." << endl; // indicate list is empty
+    cout << "Empty list." << endl; // indicate list is now empty
 
     return 0;
 }
 
-// add a node at the front
+// add a node at front
 void addToFront(Node*& head, int val) {
-    Node* newNode = new Node{val, head}; // create node pointing to current head
+    Node* newNode = new Node{val, head}; // create new node pointing to head
     head = newNode;                       // update head
 }
 
-// add a node at the tail
+// add a node at tail
 void addToTail(Node*& head, int val) {
     Node* newNode = new Node{val, nullptr}; // new node points to nullptr
     if (!head) {                             // if list empty
         head = newNode;                      // new node becomes head
         return;
     }
-    Node* temp = head; // traverse to end
+    Node* temp = head; // traverse to last node
     while (temp->next) temp = temp->next; // move to last node
     temp->next = newNode; // link last node to new node
 }
 
-// delete a node at a given position
+// insert a node after a given position
+void insertNode(Node*& head, int pos, int val) {
+    Node* newNode = new Node{val, nullptr}; // create new node
+    if (pos == 0) {                          // if position is 0, insert at head
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+    Node* temp = head; // traverse to given position
+    for (int i = 1; i < pos && temp; i++) temp = temp->next;
+    if (temp) { // insert node
+        newNode->next = temp->next;
+        temp->next = newNode;
+    } else {
+        delete newNode; // position invalid, delete node
+    }
+}
+
+// delete a node at given position
 void deleteNode(Node*& head, int pos) {
     if (!head) return; // nothing to delete
     if (pos == 1) {   // delete first node
@@ -85,7 +114,7 @@ void deleteNode(Node*& head, int pos) {
     }
 }
 
-// display the linked list
+// display linked list
 void output(Node* head) {
     if (!head) return; // nothing to display
     int count = 1;
